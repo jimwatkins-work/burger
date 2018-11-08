d405755945076930cb5b826c1b49c174c0463860
Jim Watkins <jimwatkins@Watkins-Jim-20000147.local>
2018-11-08 10:27:25 -0800

    added dynamic styling to sidebar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,19 @@
 import React from "react";
-import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.module.css";
 
-const toolbar = props => {
+const sideDrawer = props => {
   return (
-    <header className={classes.Toolbar}>
-      <div>Menu</div>
+    <div className={classes.SideDrawer}>
       <div className={classes.Logo}>
         <Logo />
       </div>
-      <nav className={classes.DesktopOnly}>
+      <nav>
         <NavigationItems />
       </nav>
-    </header>
+    </div>
   );
 };
 
-export default toolbar;
+export default sideDrawer;
