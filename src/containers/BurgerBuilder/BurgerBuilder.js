9ff3dbd47b6677188f8791ebff548b2d507032f4
Jim Watkins <jimwatkins@Watkins-Jim-20000147.local>
2018-11-14 15:58:25 -0800

    integrated loading spinner for server requests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 class BurgerBuilder extends Component {
   state = {
@@ -23,7 +25,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 6.5,
     purchasable: false,
-    purchasing: false
+    purchasing: false,
+    loading: false
   };
 
   updatePurchaseState(ingredients) {
@@ -69,7 +72,30 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("The order is on it's way");
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Jim Watkins",
+        address: {
+          street: "15154 Bunker Hill Ct",
+          city: "Happy Valley",
+          state: "OR",
+          zip: "97086"
+        },
+        email: "james.watkins77@yahoo.com"
+      },
+      deliveryMethod: "fastest"
+    };
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        this.setState({ loading: false, purchasing: false });
+      })
+      .catch(error => {
+        this.setState({ loading: false, purchasing: false });
+      });
   };
 
   render() {
@@ -81,20 +107,28 @@ class BurgerBuilder extends Component {
       disabledInfo[key] = disabledInfo[key] <= 0;
     }
 
+    let orderSummary = (
+      <OrderSummary
+        ingredientPrices={this.state.prices}
+        ingredients={this.state.ingredients}
+        price={this.state.totalPrice}
+        subtotal={this.state.prices.burger}
+        cancelPurchase={this.purchaseCancelHandler}
+        continuePurchase={this.purchaseContinueHandler}
+      />
+    );
+
+    if (this.state.loading) {
+      orderSummary = <Spinner />;
+    }
+
     return (
       <>
         <Modal
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary
-            ingredientPrices={this.state.prices}
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            subtotal={this.state.prices.burger}
-            cancelPurchase={this.purchaseCancelHandler}
-            continuePurchase={this.purchaseContinueHandler}
-          />
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
