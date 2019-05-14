6e5b96f73a2672c9358e9c70d16cbe70fa237c95
Jim Watkins <jimwatkins@Watkins-Jim-20000147.local>
2019-05-14 13:12:45 -0700

    allure

--- wdio.conf.js
+++ wdio.conf.js
@@ -121,7 +121,15 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter.html
-  reporters: ["dot"],
+  reporters: ["dot", "allure"],
+  reporterOptions: {
+    allure: {
+      outputDir: "allure-results",
+      disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: true,
+      useCucumberStepReporter: false
+    }
+  },
   //
   // Options to be passed to Mocha.
   // See the full list at http://mochajs.org/
